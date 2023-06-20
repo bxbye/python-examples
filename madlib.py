6c9cc332f82a6867a3a3d712315976c1b59929b9
Kadir Kaya <kadirkaya2050@gmail.com>
2023-06-20 08:48:01 +0300

    get input from user examples trained in login.py and madlib.py files.

---
+++ madlib.py
@@ -0,0 +1,11 @@
+"""
+Python sample project examples from https://github.com/kying18/beginner-projects/blob/master/madlibs.py
+"""
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \n I love to {verb1}. Stay hydrated and {verb2} as you are {famous_person}!"
+print(madlib)
